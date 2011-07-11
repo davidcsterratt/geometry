08a4bf84bee0763ca311886a7c22c3a7e4663cb3
dcs <dcs@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-11 10:08:16 +0000

    General tidying of the web page
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/geometry@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -7,6 +7,7 @@ $domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
 $group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
 $themeroot='http://r-forge.r-project.org/themes/rforge/'; ?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
 <head>
 <meta name="generator" content=
 "HTML Tidy for Linux (vers 25 March 2009), see www.w3.org" />
@@ -16,23 +17,11 @@ $themeroot='http://r-forge.r-project.org/themes/rforge/'; ?>
 <link href="%3C?php%20echo%20$themeroot;%20?%3Estyles/estilo1.css"
 rel="stylesheet" type="text/css" />
 <style type='text/css'>
-/*<![CDATA[*/
- /* met dit stylesheet is de opmaak van de vragen gemakkelijk aan te passen */
-
- .vraag            { border-style:none; border-width: 0px; border-color: black; margin: 2px;} /* bepaald de overall opmaak, inclusief de rand*/
- .vraagwrapper     { };                                                                        /* omsluit de vraag, nuttig voor bijvoorbeeld een tweede rand */
- .vraagrow1        { vertical-align : top; text-align: left;}                                  /* bepaald opmaak van de vraag tekst*/
- .vraagresponsen   { text-align: left;}                                                        /* opmaak van de respons-regel */
- .vraagtable       { border-style: none; }                                                     /* opmaak van de table (iedere vraag staat in een table) */
- .SQWtd            { height: 1em; }                                                            /* opmaak van alle table-cellen van de respons-regel */
- .SQWtextarea      { font-family: verdana; }                                                   /* opmaak van het invoerveld bij open vragen */
- .SQWtext          { border-style: solid; }                                                    /* opmaak van het invoerveld bij 'text' vragen */
- .SQWradio         { border-style: solid; }                                                    /* opmaak van het invoerveld bij 'Likert' vragen */
- .SQWcheckbox      { border-style: solid; }                                                    /* opmaak van het invoerveld bij 'check' vragen */
- .submitButton     { border-style: none; font-size: 8pt;}                                      /* bepaald opmaak van de submit-button */
+  /*<![CDATA[*/
 /*]]>*/
 </style>
 </head>
+
 <body>
 <!-- R-Forge Logo -->
 <table border="0" width="100%" cellspacing="0" cellpadding="0">
@@ -42,6 +31,7 @@ rel="stylesheet" type="text/css" />
 border="0" alt="R-Forge Logo" /></a></td>
 </tr>
 </table>
+
 <!-- get project title  -->
 <!-- own website starts here, the following may be changed as you like -->
 <?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
@@ -51,7 +41,9 @@ while (!feof($handle)) {
 }
 fclose($handle);
 echo $contents; } ?><!-- end of project description -->
+
 <h1>Qhull in R</h1>
+
 <p>The geometry package intents to implement several geometry
 functions available in Octave and Matlab and the like, available in
 <a href=
@@ -61,24 +53,33 @@ qhull library (which also underlies the corresponding Matlab and
 Octave functions). Currently only part of the qhull library is
 accessible and no specific class for a Delaunay triangulation is
 specified.</p>
+
 <p>The geometry package also implements a simple technique to
 refine a given mesh and generate high quality meshes and surface
 triangulations. A description of the technique and a number of
 examples can be found at the site of its inventors <a href=
 "http://web.archive.org/web/20080218222024/http://www-math.mit.edu/~persson/mesh/">
 Per-Olof Persson and Gilbert Strang</a>.</p>
+
 <p>The Qhull library is documented online at <a href=
-"http://web.archive.org/web/20080218222024/http://www.qhull.org/">www.qhull.org</a></p>
-<p>A <a href=
-"http://web.archive.org/web/20080218222024/http://cran.at.r-project.org/src/contrib/geometry_0.0-2.tar.gz">
-gzipped tar-ball</a> with the sources, and a pre-compiled binary
-distribution for windows can be downloaded from CRAN. All original
-qhull sources are included in the distribution. The distribution is
-subject to <a href=
-"http://web.archive.org/web/20080218222024/http://www.qhull.org/COPYING.txt">
-this license</a>. The non-qhull sources are subject to the GPL
-version 2 or higher.<br /></p>
+"http://www.qhull.org/">www.qhull.org</a></p>
+
+<p>A gzipped tar-ball with the sources, and a pre-compiled binary
+distribution for windows can be downloaded
+from <a href="http://cran.r-project.org/web/packages/geometry/index.html">CRAN</a>. All
+original qhull sources are included in the distribution. The
+distribution is subject to <a href=
+"http://www.qhull.org/COPYING.txt">
+this license</a>. The non-qhull sources are subject to the GPL version
+2 or higher.<br /></p>
+
+<p>The code repository is hosted on R-Forge. To find the latest
+  snapshot of the package, go to the <a href=
+  "http://%3C?php%20echo%20$domain;%20?%3E/projects/%3C?php%20echo%20$group_name;%20?%3E/">
+  R-Forge project summary page</a>.</p>
+
 <h2>Using the Qhull options</h2>
+
 <p>Qhull provides many options that you can use to your advantage.
 For instance you may request the volume of a convex hull and its
 surface area by specifying the 'FS' option. Currently the output is
@@ -87,13 +88,12 @@ programmatically. However, Qhull has the option TO
 <em>filename</em> which can be used to write the output to a file
 which can then be parsed into R again. For instance, if you want to
 compute the volume of a convex hull of a set of points, if you have
-a matrix with 3 columns, <span style=
-"color:red;font-family: Courier;font-size: 10pt;"><br />
-&gt; ps &lt;- matrix(rnorm(30),,3);</span><br />
-say, that might be something like <span style=
-"color:red;font-family: Courier;font-size: 10pt;"><br />
-&gt; x = convhulln(ps,"FS TO 'bla.txt'"); # use of the 'TO
-filename' option</span><br />
+a matrix with 3 columns,
+<pre>&gt; ps &lt;- matrix(rnorm(30),,3);
+</pre>
+say, that might be something like
+<pre>&gt; x = convhulln(ps,"FS TO 'bla.txt'"); # use of the 'TO filename' option
+</pre>
 This command created the text file 'bla.txt' that contains the
 requested output (i.e. the output specified by the Qhull options).
 For the Qhull option FS this is the following: [<a href=
@@ -103,11 +103,13 @@ the doc</a>] "The first line consists of the number of integers
 followed by the total facet area, and the total volume. Later
 versions of Qhull may produce additional integers or reals." You
 can then parse the text in the file according to your needs; in
-your case it will be something like <span style=
-"color:red;font-family: Courier;font-size: 10pt;"><br />
-&gt; qh.outp &lt;- scan('bla.txt ');<br />
-&gt; volume &lt;- qh.outp[3];</span><br /></p>
+your case it will be something like 
+<pre>&gt; qh.outp &lt;- scan('bla.txt ');
+&gt; volume &lt;- qh.outp[3];</pre>
+</p>
+
 <h2>Wish list for future releases</h2>
+
 <ul>
 <li>port __vonoroi__.cc</li>
 <li>get rid of output to console in convhulln (specifying NULL as
@@ -119,14 +121,14 @@ standard output handle gives wrong result)</li>
 Per-Olof Persson (http://www-math.mit.edu/~persson/mesh/)</li>
 <li>n-D interpolation with Delaunay triangulation</li>
 </ul>
+
 <h2>Contribute</h2>
+
 <p>If you have ideas for functions, or functions you made and think
 would be of value to include in the geometry package (take into
 account that the package is called <code>geometry</code> and
 restricts itself to functions directly related to computational
 geometry), you can e-mail me <a href="mailto:david.c.sterratt@ed.ac.uk">david.c.sterratt@ed.ac.uk</a>.</p>
-<p>The <strong>project summary page</strong> you can find <a href=
-"http://%3C?php%20echo%20$domain;%20?%3E/projects/%3C?php%20echo%20$group_name;%20?%3E/">
-<strong>here</strong></a>.</p>
+
 </body>
 </html>
