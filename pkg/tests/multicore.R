50711538af8bb9a43b1c0375527e4df30f4fecee
dcs <dcs@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-18 17:11:19 +0000

    Added multicore test
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/geometry@39 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/tests/multicore.R
@@ -0,0 +1,16 @@
+library(geometry)
+library(multicore)
+
+## Set seed for replicability
+set.seed(1)
+
+## Create points and try standard Delaunay Triangulation
+N <- 100000
+P <- matrix(runif(2*N), N, 2)
+T <- delaunayn(P)
+print(nrow(T))
+
+## Now try out the parallel version
+Ts <- mclapply(list(P, P, P, P), delaunayn)
+print(length(Ts))
+print(nrow(Ts[[1]]))
