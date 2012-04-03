ca0d4e6fb61a5bfead1298d394aba646cd9051d6
dcs <dcs@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-04-03 12:11:47 +0000

    * Change in order of exported functions
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/geometry@67 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/multicore.R
+++ pkg/tests/multicore.R
@@ -1,16 +1,17 @@
 library(geometry)
-library(multicore)
+## multicore isn't available on all operating systems, e.g. Windows
+if (library(multicore, logical.return=TRUE)) {
+  ## Set seed for replicability
+  set.seed(1)
 
-## Set seed for replicability
-set.seed(1)
+  ## Create points and try standard Delaunay Triangulation
+  N <- 100000
+  P <- matrix(runif(2*N), N, 2)
+  T <- delaunayn(P)
+  print(nrow(T))
 
-## Create points and try standard Delaunay Triangulation
-N <- 100000
-P <- matrix(runif(2*N), N, 2)
-T <- delaunayn(P)
-print(nrow(T))
-
-## Now try out the parallel version
-Ts <- mclapply(list(P, P, P, P), delaunayn)
-print(length(Ts))
-print(nrow(Ts[[1]]))
+  ## Now try out the parallel version
+  Ts <- mclapply(list(P, P, P, P), delaunayn)
+  print(length(Ts))
+  print(nrow(Ts[[1]]))
+}
