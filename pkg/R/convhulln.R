9a1fe2c51c5db7bb8843fc35c7e14e38cc3c103b
dcs <dcs@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-04-03 12:45:28 +0000

    * Fix for Issue #1924: Silent operation is not supported
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/geometry@69 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/convhulln.R
+++ pkg/R/convhulln.R
@@ -1,41 +1,33 @@
-##' Compute smallest convex hull that encloses a set of points
+##' @title Compute smallest convex hull that encloses a set of points
 ##' 
 ##' Returns an index matrix to the points of simplices
 ##' (\dQuote{triangles}) that form the smallest convex simplicial
-##' complex of a set of input points in N-dimensional space.
+##' complex of a set of input points in N-dimensional space. This
+##' function interfaces the Qhull library.
 ##' 
-##' This function interfaces the qhull library and is a port from
-##' Octave to R.
-##' 
-##' The input \code{n}-by-\code{dim} matrix contains \code{n} points
-##' of dimension \code{dim}. If a second optional argument is given,
-##' it must be a string containing extra options for the underlying
-##' qhull command.  
-##' 
+##' For slient operation, specify the option \code{Pp}. 
+##'
 ##' @param p An \code{n}-by-\code{dim} matrix.  The rows of \code{p} represent
 ##' \code{n} points in \code{dim}-dimensional space.
-##' @param options Optional options, see details below and Qhull
-##' documentation at
-##' \url{http://www.qhull.org/html/qconvex.htm#synopsis}.  The
-##' \code{options}  \code{Qt}.
+##' @param options String containing extra options for the underlying
+##' Qhull command; see details below and Qhull documentation at
+##' \url{http://www.qhull.org/html/qconvex.htm#synopsis}.
 ##' 
-##' @return An \code{m}-by-\code{dim} index matrix of which each row defines a
-##' \code{dim}-dimensional \dQuote{triangle}. The indices refer to the rows in
-##' \code{p}.  If the option \code{"FA"} is provided, then the output is a
-##' \code{list} with entries \code{$hull} containing the matrix mentioned
-##' above, and \code{$area} and \code{$vol} with the area and volume of the
-##' hull described by the matrix.
-##' @note This intents to be a port of the Octave's
-##' (\url{http://www.octave.org}) geometry library. The sources originals were
-##' from Kai Habel.
+##' @return An \code{m}-by-\code{dim} index matrix of which each row
+##' defines a \code{dim}-dimensional \dQuote{triangle}. The indices
+##' refer to the rows in \code{p}.  If the option \code{FA} is
+##' provided, then the output is a \code{list} with entries
+##' \code{$hull} containing the matrix mentioned above, and
+##' \code{$area} and \code{$vol} with the area and volume of the hull
+##' described by the matrix.
 ##'
-##' To get the usual progress-related output specify the R-specific
-##' option \code{"Pp Ps"}. The option \code{"FA"} results in the area
-##' and volume of the convex hull to be included in the output list.
+##' @note This is a port of the Octave's (\url{http://www.octave.org})
+##' geometry library. The Octave source was written by Kai Habel.
 ##' 
-##' See further notes in \link{delaunayn}.
-##' @author Raoul Grasman and Robert B. Gramacy
-##' \email{bobby@@statslab.cam.ac.uk}
+##' See further notes in \code{\link{delaunayn}}.
+##' 
+##' @author Raoul Grasman, Robert B. Gramacy and David Sterratt
+##' \email{david.c.sterratt@@ed.ac.uk}
 ##' @seealso \code{\link[tripack]{convex.hull}}, \code{\link{delaunayn}},
 ##' \code{\link{surf.tri}}, \code{\link{distmesh2d}}
 ##' @references \cite{Barber, C.B., Dobkin, D.P., and Huhdanpaa, H.T.,
@@ -47,9 +39,9 @@
 ##' @examples
 ##' # example convhulln
 ##' # ==> see also surf.tri to avoid unwanted messages printed to the console by qhull
-##' ps = matrix(rnorm(3000),ncol=3)                     # generate points on a sphere
-##' ps = sqrt(3) * ps / drop(sqrt((ps^2) %*% rep(1,3)))
-##' ts.surf = t( convhulln(ps,"QJ") ) # see the qhull documentations for the options
+##' ps <- matrix(rnorm(3000), ncol=3)  # generate points on a sphere
+##' ps <- sqrt(3)*ps/drop(sqrt((ps^2) %*% rep(1, 3)))
+##' ts.surf <- t(convhulln(ps, "QJ"))  # see the qhull documentations for the options
 ##' \dontrun{
 ##' rgl.triangles(ps[ts.surf,1],ps[ts.surf,2],ps[ts.surf,3],col="blue",alpha=.2)
 ##' for(i in 1:(8*360)) rgl.viewpoint(i/8)
@@ -58,5 +50,15 @@
 ##' @export
 ##' @useDynLib geometry
 convhulln <- function (p, options = "Tv") {
+  ## Input sanitisation
+  options <- paste(options, collapse=" ")
+
+  ## It is essential that delaunayn is called with either the QJ or Qt
+  ## option. Otherwise it may return a non-triangulated structure, i.e
+  ## one with more than dim+1 points per structure, where dim is the
+  ## dimension in which the points p reside.
+  if (!grepl("Qt", options) & !grepl("QJ", options)) {
+    options <- paste(options, "Qt")
+  }
   .Call("convhulln", as.matrix(p), as.character(options), PACKAGE="geometry")
 }
