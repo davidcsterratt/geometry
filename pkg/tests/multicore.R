0fbbc49ada15232e9007aae0aee3ea6e3cb390f8
dcs <dcs@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-04-15 10:31:40 +0000

    * multicore test only uses 2 cores, as per CRAN instructions
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/geometry@75 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/multicore.R
+++ pkg/tests/multicore.R
@@ -10,8 +10,9 @@ if (library(multicore, logical.return=TRUE)) {
   T <- delaunayn(P)
   print(nrow(T))
 
-  ## Now try out the parallel version
-  Ts <- mclapply(list(P, P, P, P), delaunayn)
+  ## Now try out the parallel version. Use only 2 cores to comply with
+  ## CRAN guidelines.
+  Ts <- mclapply(list(P, P, P, P), delaunayn, mc.cores=2)
   print(length(Ts))
   print(nrow(Ts[[1]]))
 }
