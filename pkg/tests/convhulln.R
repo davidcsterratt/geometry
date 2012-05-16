5712db1c881b613b65a511d7c82747f01f633471
dcs <dcs@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-16 09:31:14 +0000

    * Tweaks to tests
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/geometry@101 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/convhulln.R
+++ pkg/tests/convhulln.R
@@ -1,7 +1,7 @@
 library(geometry)
 set.seed(1)
-ps <- matrix(rnorm(3000),ncol=3)
-ps <- sqrt(3) * ps / drop(sqrt((ps^2) %*% rep(1,3)))
+ps <- matrix(rnorm(3000), ncol=3)
+ps <- sqrt(3)*ps/drop(sqrt((ps^2) %*% rep(1,3)))
 ts <- convhulln(ps)
 print(ts)
 
@@ -12,3 +12,7 @@ try(convhulln(ps))
 ## This should throw an error, but not segfault
 try(convhulln(diag(4)))
 
+## Output to file should work
+ps <- matrix(rnorm(3000), ncol=3)
+ps <- sqrt(3)*ps/drop(sqrt((ps^2) %*% rep(1, 3)))
+ts <- convhulln(ps, "TO 'test.txt'")
