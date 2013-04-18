102d1dfdca7502131c543dab2a36eba70d1b995f
dcs <dcs@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-18 09:17:40 +0000

    Modification to fix problems with parallel test on Windows
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/geometry@120 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/parallel.R
+++ pkg/tests/parallel.R
@@ -1,18 +1,20 @@
 library(geometry)
-## Parallel may not be available on all operating systems, e.g. Windows
-if (library(parallel, logical.return=TRUE)) {
-  ## Set seed for replicability
-  set.seed(1)
+library(parallel)
 
-  ## Create points and try standard Delaunay Triangulation
-  N <- 100000
-  P <- matrix(runif(2*N), N, 2)
-  T <- delaunayn(P)
-  print(nrow(T))
+## mc.cores must be 1 on Windows. Otherwise use only 2 cores to comply
+## with CRAN guidelines.
+mc.cores <- ifelse(Sys.info()[1] == "Windows", 1, 2)
 
-  ## Now try out the parallel version. Use only 2 cores to comply with
-  ## CRAN guidelines.
-  Ts <- mclapply(list(P, P, P, P), delaunayn, mc.cores=2)
-  print(length(Ts))
-  print(nrow(Ts[[1]]))
-}
+## Set seed for replicability
+set.seed(1)
+
+## Create points and try standard Delaunay Triangulation
+N <- 100000
+P <- matrix(runif(2*N), N, 2)
+T <- delaunayn(P)
+print(nrow(T))
+
+## Now try out the parallel version. 
+Ts <- mclapply(list(P, P, P, P), delaunayn, mc.cores=mc.cores)
+print(length(Ts))
+print(nrow(Ts[[1]]))
