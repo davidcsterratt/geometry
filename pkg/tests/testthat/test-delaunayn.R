1c680746ab8c19ebca7bd4f7e3f578a4fbe70272
David C Sterratt <david.c.sterratt@ed.ac.uk>
2017-04-28 13:48:15 +0100

    Fixed typo

--- pkg/tests/testthat/test-delaunayn.R
+++ pkg/tests/testthat/test-delaunayn.R
@@ -75,7 +75,7 @@ test_that("The QJ option can give degenerate simplices", {
 })
 
 test_that("A square is triangulated", {
-  ## This doesn't work if the Qz options isn't supplied
+  ## This doesn't work if the Qz option isn't supplied
   square <- rbind(c(0, 0), c(0, 1), c(1, 0), c(1, 1))
   expect_that(delaunayn(square), equals(rbind(c(4, 2, 1),
                                               c(4, 3, 1))))
