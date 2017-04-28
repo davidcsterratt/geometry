d145ef90a745590e8c294c4971e55c3bcee30b24
David C Sterratt <david.c.sterratt@ed.ac.uk>
2017-04-28 17:29:41 +0100

    Issue #6: Return qhull objects from C_delaunayn()
    The code has been modified in a similar way to the C_convhulln() code,
    by adding a finalizer that cleans up the memory when the object is
    finally destroyed.

--- pkg/src/Rdelaunayn.c
+++ pkg/src/Rdelaunayn.c
@@ -36,6 +36,25 @@
 #include "qhull_ra.h"
 #include <unistd.h>              /* For unlink() */
 
+/* Finalizer which R will call when garbage collecting. This is
+   registered at the end of delaunaynn() */
+static void delaunaynFinalizer(SEXP ptr)
+{
+  int curlong, totlong;
+  if(!R_ExternalPtrAddr(ptr)) return;
+  qhT *qh;
+  qh = R_ExternalPtrAddr(ptr);
+
+  qh_freeqhull(qh, !qh_ALL);                /* free long memory */
+  qh_memfreeshort (qh, &curlong, &totlong);	/* free short memory and memory allocator */
+  if (curlong || totlong) {
+    warning("delaunaynn: did not free %d bytes of long memory (%d pieces)",
+	    totlong, curlong);
+  }
+  qh_free(qh);
+  R_ClearExternalPtr(ptr); /* not really needed */
+}
+
 SEXP C_delaunayn(const SEXP p, const SEXP options, SEXP tmpdir)
 {
   SEXP retlist, retnames;       /* Return list and names */
@@ -43,8 +62,9 @@ SEXP C_delaunayn(const SEXP p, const SEXP options, SEXP tmpdir)
 	SEXP tri;                     /* The triangulation */
   SEXP neighbour, neighbours;   /* List of neighbours */
   SEXP areas;                    /* Facet areas */
-	int i;
+	int i, j;
 	unsigned dim, n;
+  int exitcode = 1;
 	boolT ismalloc;
 	char flags[250];             /* option flags for qhull, see qh_opt.htm */
 	double *pt_array;
@@ -86,125 +106,109 @@ SEXP C_delaunayn(const SEXP p, const SEXP options, SEXP tmpdir)
     error("Number of points is not greater than the number of dimensions.");
   }
 
-	if (n > dim + 1) {
-		int i, j;
-		int exitcode = 1;
-		int curlong, totlong;
+  i = 0, j = 0;
+  pt_array = (double *) R_alloc(n*dim, sizeof(double)); 
+  for(i=0; i < n; i++)
+    for(j=0; j < dim; j++)
+      pt_array[dim*i+j] = REAL(p)[i+n*j];
+  ismalloc = False;   /* True if qhull should free points in qh_freeqhull() or reallocation */
 
-		i=0,j=0;
-		pt_array = (double *) R_alloc(n*dim, sizeof(double)); 
-		for(i=0; i < n; i++)
-			for(j=0; j < dim; j++)
-				pt_array[dim*i+j] = REAL(p)[i+n*j];
-		ismalloc = False;   /* True if qhull should free points in qh_freeqhull() or reallocation */
+  /* Jiggery-pokery to create and destroy the ersatz stdout, and the
+     call to qhull itself. */    
+  const char *name;
+  name = R_tmpnam("Rf", CHAR(STRING_ELT(tmpdir, 0)));
+  tmpstdout = fopen(name, "w");
+  qhT *qh= (qhT*)malloc(sizeof(qhT));
+  qh_zero(qh, errfile);
+  exitcode = qh_new_qhull(qh, dim, n, pt_array, ismalloc, flags, tmpstdout, errfile); 
+  fclose(tmpstdout);
+  unlink(name);
+  free((char *) name); 
 
-    /* Jiggery-pokery to create and destroy the ersatz stdout, and the
-       call to qhull itself. */    
-    const char *name;
-    name = R_tmpnam("Rf", CHAR(STRING_ELT(tmpdir, 0)));
-    tmpstdout = fopen(name, "w");
-    qhT *qh= (qhT*)malloc(sizeof(qhT));
-    qh_zero(qh, errfile);
-		exitcode = qh_new_qhull(qh, dim, n, pt_array, ismalloc, flags, tmpstdout, errfile); 
-    fclose(tmpstdout);
-    unlink(name);
-    free((char *) name); 
-    if (exitcode) {
-      error ("non-zero exitcode");
+  if (!exitcode) {                    /* 0 if no error from qhull */
+    /* Triangulate non-simplicial facets - this commented out code
+       does not appear to be needed, but retaining in case useful --
+       David Sterratt, 2013-04-17 */
+    /* qh_triangulate (); */
+
+    facetT *facet;                  /* set by FORALLfacets */
+    vertexT *vertex, **vertexp;
+    facetT *neighbor, **neighborp;
+
+    /* Count the number of facets so we know how much space to
+       allocate in R */
+    int nf=0;                 /* Number of facets */
+    FORALLfacets {
+      if (!facet->upperdelaunay) {
+        nf++;
+      }
+      /* Double check. Non-simplicial facets will cause segfault
+         below */
+      if (! facet->simplicial) {
+        Rprintf("Qhull returned non-simplicial facets -- try delaunayn with different options");
+        exitcode = 1;
+        break;
+      }
     }
-		if (!exitcode) {                    /* 0 if no error from qhull */
-      /* Triangulate non-simplicial facets - this commented out code
-         does not appear to be needed, but retaining in case useful --
-         David Sterratt, 2013-04-17 */
-      /* qh_triangulate (); */
-
-			facetT *facet;                  /* set by FORALLfacets */
-			vertexT *vertex, **vertexp;
-      facetT *neighbor, **neighborp;
-
-      /* Count the number of facets so we know how much space to
-         allocate in R */
-			int nf=0;                 /* Number of facets */
-			FORALLfacets {
-				if (!facet->upperdelaunay) {
-          nf++;
-        }
-        /* Double check. Non-simplicial facets will cause segfault
-           below */
-        if (! facet->simplicial) {
-          error ("Qhull returned non-simplicial facets -- try delaunayn with different options");
-          exitcode = 1;
-          break;
-        }
-			}
       
-      /* Alocate the space in R */
-      PROTECT(tri = allocMatrix(INTSXP, nf, dim+1));
-      PROTECT(neighbours = allocVector(VECSXP, nf));
-      PROTECT(areas = allocVector(REALSXP, nf));
+    /* Alocate the space in R */
+    PROTECT(tri = allocMatrix(INTSXP, nf, dim+1));
+    PROTECT(neighbours = allocVector(VECSXP, nf));
+    PROTECT(areas = allocVector(REALSXP, nf));
 
-      /* Iterate through facets to extract information */
-      int i=0;
-			FORALLfacets {
-				if (!facet->upperdelaunay) {
-          if (i >= nf) {
-            error("Trying to access non-existent facet %i", i);
-          }
+    /* Iterate through facets to extract information */
+    int i=0;
+    FORALLfacets {
+      if (!facet->upperdelaunay) {
+        if (i >= nf) {
+          error("Trying to access non-existent facet %i", i);
+        }
 
-          /* Triangulation */
-					int j=0;
-					FOREACHvertex_ (facet->vertices) {
-            if ((i + nf*j) >= nf*(dim+1))
-              error("Trying to write to non-existent area of memory i=%i, j=%i, nf=%i, dim=%i", i, j, nf, dim);
-            INTEGER(tri)[i + nf*j] = 1 + qh_pointid(qh, vertex->point);
-            j++;
-					}
+        /* Triangulation */
+        int j=0;
+        FOREACHvertex_ (facet->vertices) {
+          if ((i + nf*j) >= nf*(dim+1))
+            error("Trying to write to non-existent area of memory i=%i, j=%i, nf=%i, dim=%i", i, j, nf, dim);
+          INTEGER(tri)[i + nf*j] = 1 + qh_pointid(qh, vertex->point);
+          j++;
+        }
 
-          /* Neighbours */
-          PROTECT(neighbour = allocVector(INTSXP, qh_setsize(qh, facet->neighbors)));
-          j=0;
-          FOREACHneighbor_(facet) {
-            INTEGER(neighbour)[j] = neighbor->visitid ? neighbor->visitid: 0 - neighbor->id;
-            j++;
-          }
-          SET_VECTOR_ELT(neighbours, i, neighbour);
-          UNPROTECT(1);
+        /* Neighbours */
+        PROTECT(neighbour = allocVector(INTSXP, qh_setsize(qh, facet->neighbors)));
+        j=0;
+        FOREACHneighbor_(facet) {
+          INTEGER(neighbour)[j] = neighbor->visitid ? neighbor->visitid: 0 - neighbor->id;
+          j++;
+        }
+        SET_VECTOR_ELT(neighbours, i, neighbour);
+        UNPROTECT(1);
           
-          /* Area. Code modified from qh_getarea() in libquhull/geom2.c */
-          if ((facet->normal) && !(facet->upperdelaunay && qh->ATinfinity)) {
-            if (!facet->isarea) {
-              facet->f.area= qh_facetarea(qh, facet);
-              facet->isarea= True;
-            }
-            REAL(areas)[i] = facet->f.area;
+        /* Area. Code modified from qh_getarea() in libquhull/geom2.c */
+        if ((facet->normal) && !(facet->upperdelaunay && qh->ATinfinity)) {
+          if (!facet->isarea) {
+            facet->f.area= qh_facetarea(qh, facet);
+            facet->isarea= True;
           }
+          REAL(areas)[i] = facet->f.area;
+        }
 
-					i++;
-				}
-			}
-      UNPROTECT(3);
-		} 
-    
-    /* Do cleanup regardless of whether there is an error */
-		qh_freeqhull(qh, !qh_ALL);                  /* free long memory */
-		qh_memfreeshort (qh, &curlong, &totlong);   /* free short memory and memory allocator */
-    qh_free(qh);
-    
-		if (curlong || totlong) {
-			warning("delaunay: did not free %d bytes of long memory (%d pieces)", totlong, curlong);
-		}
-    if (exitcode) {
-			error("Received error code %d from qhull.", exitcode);
-		}
-	} else if (n == dim + 1) {
-    /* Number of points is one more than the number of dimensions. */
-		/* FIXME. Need to check if nx points span a simplex. */
-    PROTECT(tri = allocMatrix(INTSXP, 1, dim+1));
-		for (int i=0; i<n; i++) {
-			INTEGER(tri)[i] = i + 1;
-		}
-		UNPROTECT(1);
-	}
+        i++;
+      }
+    }
+    UNPROTECT(3);
+  } else { /* exitcode != 1 */
+    /* There has been an error; Qhull will print the error
+       message */
+    PROTECT(tri = allocMatrix(INTSXP, 0, dim+1)); 
+    UNPROTECT(1);
+    /* If the error been because the points are colinear, coplanar
+       &c., then avoid mentioning an error by setting exitcode=2*/
+    /* Rprintf("dim %d; n %d\n", dim, n); */
+    if ((dim + 1) == n) {
+      exitcode = 2;
+    }
+  }
+
   PROTECT(retlist = allocVector(VECSXP, retlen));
   PROTECT(retnames = allocVector(VECSXP, retlen));
   SET_VECTOR_ELT(retlist, 0, tri);
@@ -216,6 +220,25 @@ SEXP C_delaunayn(const SEXP p, const SEXP options, SEXP tmpdir)
   setAttrib(retlist, R_NamesSymbol, retnames);
   UNPROTECT(2);
 
+  /* Register delaunaynFinalizer() for garbage collection and attach a
+     pointer to the hull as an attribute for future use. */
+  SEXP ptr, tag;
+  PROTECT(tag = allocVector(STRSXP, 1));
+  SET_STRING_ELT(tag, 0, mkChar("delaunayTriangulation"));
+  PROTECT(ptr = R_MakeExternalPtr(qh, tag, R_NilValue));
+  if (exitcode) {
+    delaunaynFinalizer(ptr);
+  } else {
+    R_RegisterCFinalizerEx(ptr, delaunaynFinalizer, TRUE);
+    setAttrib(retlist, tag, ptr);
+  }
+  UNPROTECT(2);
+  
+  if (exitcode & (exitcode != 2)) {
+    error("Received error code %d from qhull.", exitcode);
+  } 
+
+  
 	return retlist;
 }
 
