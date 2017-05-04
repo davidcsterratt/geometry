93a970bc4af6af02998a396df10931da54b47ae4
Jean-Romain <jromain.roussel@gmail.com>
2017-05-04 05:44:15 -0400

    Rewrite tsearch with QuadTree

--- pkg/src/geometry_init.c
+++ pkg/src/geometry_init.c
@@ -11,13 +11,13 @@
 extern SEXP C_convhulln(SEXP, SEXP, SEXP);
 extern SEXP C_delaunayn(SEXP, SEXP, SEXP);
 extern SEXP C_inhulln(SEXP, SEXP);
-extern SEXP C_tsearch(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP geometry_C_tsearch(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
     {"C_convhulln", (DL_FUNC) &C_convhulln, 3},
     {"C_delaunayn", (DL_FUNC) &C_delaunayn, 3},
     {"C_inhulln",   (DL_FUNC) &C_inhulln,   2},
-    {"C_tsearch",   (DL_FUNC) &C_tsearch,   6},
+    {"geometry_C_tsearch",   (DL_FUNC) &geometry_C_tsearch,   6},
     {NULL, NULL, 0}
 };
 
