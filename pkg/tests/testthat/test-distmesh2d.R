a9e646431fb5c3da5ca100fb3b4b9e29b6142974
David C Sterratt <david.c.sterratt@ed.ac.uk>
2017-09-11 16:41:09 +0100

    Issue #15: Make distmesh2d() work more silently when not interactive
    The new plot option (TRUE by default) allows plotting to during mesh
    generation to be suspended, which is useful for running tests. The use
    of cat() has been replaced by message(), meaning messages can be
    supressed, for example during tests.

--- pkg/tests/testthat/test-distmesh2d.R
+++ pkg/tests/testthat/test-distmesh2d.R
@@ -8,8 +8,6 @@ test_that("distmesh2d can create a mesh on an ellipse", {
     return(sqrt(((p[,1]-xc2)/ra2)^2+((p[,2]-yc2)/rb2)^2)-1)
   }
   ## Solve using distmesh2d()
-  scan(quiet=TRUE)
   fh <- function(p,...)  rep(1,nrow(p))
-  p <- distmesh2d(fd=fd1,fh=fh,p=NULL,h0=0.05,bbox=bbox,maxiter=1000)
+  expect_warning(p <- distmesh2d(fd=fd1,fh=fh,p=NULL,h0=0.05,bbox=bbox,maxiter=1000, plot=FALSE))
 })
-
