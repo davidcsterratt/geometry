a9993f39b341aff3f6e3c2190b8dd974ad38f585
David C. Sterratt <david.c.sterratt@ed.ac.uk>
2018-04-04 23:07:39 +0100

    Issue #26: Intersection of convex hulls - initial work

---
+++ pkg/R/intersectn.R
@@ -0,0 +1,41 @@
+##' Compute convex hull of intersection of two sets of points
+##' @param ps1 First set of points
+##' @param ps2 Second set of points
+##' @return List containing named elements: \code{ch1}, the convex
+##'   hull of the first set of points, with volumes, areas and normals
+##'   (see \code{\link{convhulln}}; \code{ch2}, the convex hull of the
+##'   first set of points, with volumes, areas and normals; \code{ps},
+##'   the intersection points of convex hulls \code{ch1} and
+##'   \code{ch2}; and \code{ch}, the convex hull of the intersection
+##'   points, with volumes, areas and normals.
+##' @export
+##' @examples
+##' # Two overlapping boxes
+##' ps1 <- rbox(0, C=0.5)
+##' ps2 <- rbox(0, C=0.5) + 0.5
+##' out <- intersectn(ps1, ps2)
+##' out$ch1$volume
+##' out$ch2$volume
+##' out$ch$volume
+##' @author David Sterratt
+##' @seealso convhulln, halfspacen, inhulln
+intersectn <- function(ps1, ps2) {
+  ## Check there are overlaps
+  ch1 <- convhulln(ps1, "n FA")
+  ch2 <- convhulln(ps2, "n FA")
+
+  in2 <- inhulln(ch2, ps1)
+  if (all(in2 == FALSE)) {
+    stop("Convex hulls of ps1 and ps2 do not overlap.")
+  }
+  in1 <- inhulln(ch1, ps2)
+
+  ## Find feasible point
+  fp <- colMeans(rbind(ps2[in1,], ps1[in2,]))
+
+  ## Find intesections of halfspaces about feasible point
+  ps <- halfspacen(rbind(ch1$normals, ch2$normals), fp)
+  ch <- convhulln(ps, "n FA")
+  
+  return(list(ch1=ch1, ch2=ch2, ps=ps, ch=ch))
+}
