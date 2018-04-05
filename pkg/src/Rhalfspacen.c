7e9c4f31029962e2b1470280d9fb470bf7e7459d
David C. Sterratt <david.c.sterratt@ed.ac.uk>
2018-04-05 10:36:18 +0100

    Issue #25: fix copyright notice

--- pkg/src/Rhalfspacen.c
+++ pkg/src/Rhalfspacen.c
@@ -1,6 +1,4 @@
-/* Copyright (C) 2000 Kai Habel
-** Copyright R-version (C) 2005 Raoul Grasman 
-**                     (C) 2013-2015, 2017 David Sterratt
+/* Copyright (C) 2018 David Sterratt
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -15,15 +13,6 @@
 ** You should have received a copy of the GNU General Public License
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
-*/
-
-/*
-29. July 2000 - Kai Habel: first release
-2002-04-22 Paul Kienzle
-* Use warning(...) function rather than writing to cerr
-
-23. May 2005 - Raoul Grasman: ported to R
-* Changed the interface for R
 */
 
 #include "Rgeometry.h"
